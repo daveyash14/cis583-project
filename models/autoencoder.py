bd2c9a8d036b40acadabeb847b29b3378cde3f93
Yash Dave <yashdave@Yashs-MacBook-Pro.local>
2024-12-08 14:45:45 -0500

    more code

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -5,11 +5,15 @@ class Encoder(nn.Module):
     def __init__(self, latent_dim):
         super(Encoder, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, latent_dim, kernel_size=4, stride=2, padding=1)
+            nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(16, latent_dim, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(),
+            # nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(),
+            # nn.Conv2d(64, latent_dim, kernel_size=3, stride=1, padding=1),
         )
 
     def forward(self, x):
@@ -20,12 +24,17 @@ class Decoder(nn.Module):
     def __init__(self, latent_dim):
         super(Decoder, self).__init__()
         self.deconv = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim, 128, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
-            nn.Tanh()
+            # nn.ConvTranspose2d(latent_dim, 64, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(),
+            # nn.ConvTranspose2d(64, 32, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(),
+            nn.ConvTranspose2d(latent_dim, 16, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(8, 1, kernel_size=3, stride=1, padding=1),
+            # nn.Tanh() # For flickr
+            nn.Sigmoid() # For mnist
         )
 
     def forward(self, x):
--- models/unet.py
+++ models/unet.py
@@ -6,7 +6,7 @@ class CrossAttention(nn.Module):
     def __init__(self, embed_dim):
         super(CrossAttention, self).__init__()
         self.attention = MultiheadAttention(embed_dim=embed_dim, num_heads=8)
-        self.latent_proj = nn.Conv2d(128, embed_dim, kernel_size=1)  # Project latent to embed_dim
+        self.latent_proj = nn.Conv2d(512, embed_dim, kernel_size=1)  # Project latent to embed_dim
         self.text_proj = nn.Linear(embed_dim, embed_dim)  # Optional: Project text embedding to embed_dim
 
     def forward(self, latent, text_embedding):
@@ -29,15 +29,23 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.down1 = nn.Conv2d(latent_dim, 64, kernel_size=3, padding=1)
         self.down2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.down3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.down4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
         self.attn = CrossAttention(embed_dim=text_embed_dim)
-        self.reduce_channels = nn.Conv2d(text_embed_dim, 128, kernel_size=1)
-        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
-        self.up2 = nn.ConvTranspose2d(64, latent_dim, kernel_size=3, padding=1)
+        self.reduce_channels = nn.Conv2d(text_embed_dim, 512, kernel_size=1)
+        self.up1 = nn.ConvTranspose2d(512, 256, kernel_size=3, padding=1)
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=3, padding=1)
+        self.up3 = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
+        self.up4 = nn.ConvTranspose2d(64, latent_dim, kernel_size=3, padding=1)
 
     def forward(self, x, text_embedding):
         x = F.relu(self.down1(x))
         x = F.relu(self.down2(x))
+        x = F.relu(self.down3(x))
+        x = F.relu(self.down4(x))
         x = self.attn(x, text_embedding)
         x = self.reduce_channels(x)
         x = F.relu(self.up1(x))
-        return self.up2(x)
+        x = F.relu(self.up2(x))
+        x = F.relu(self.up3(x))
+        return self.up4(x)
